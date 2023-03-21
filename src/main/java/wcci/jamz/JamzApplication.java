d5ec0d70bcb31643c42305fc9570bd70dbb38e1b
MarQuan McCarrel <marquanmccarrel@gmail.com>
2023-03-21 10:20:42 -0400

    Added Spring

--- src/main/java/wcci/jamz/JamzApplication.java
+++ src/main/java/wcci/jamz/JamzApplication.java
@@ -1,4 +1,4 @@
-package com.example.jamz;
+package wcci.jamz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/wcci/jamz/JamzApplicationTests.java
+++ src/test/java/wcci/jamz/JamzApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.jamz;
+package wcci.jamz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
